5bd23531934188fb168fd1f0857bce2e877f72c4
朱启凯 <qikai.zhu@sifude.com>
2019-06-26 15:28:39 +0800

    递归实现 二分查找

--- app/src/main/java/com/earnmoney/foroffer/zhu/algorithm/BinarySearchAlgorithm.java
+++ app/src/main/java/com/earnmoney/foroffer/zhu/algorithm/BinarySearchAlgorithm.java
@@ -1,7 +1,5 @@
 package com.earnmoney.foroffer.zhu.algorithm;
 
-import android.os.SystemClock;
-
 /**
  * 北京博瑞彤芸文化传播股份有限公司  版权所有
  * Copyright (c) 2019. bjbrty.com  All Rights Reserved
@@ -17,39 +15,61 @@ import android.os.SystemClock;
 
 public class BinarySearchAlgorithm {
 
-    public static void main(String[] args){
-        int[] arr = {};
-        System.out.println(binarySearch(arr,9));
-        System.out.println(binarySearch(arr,16));
+    public static void main(String[] args) {
+        int[] arr = {10, 18, 22, 38, 49, 58, 61, 70};
+//        System.out.println(binarySearch(arr, 9));
+//        System.out.println(binarySearch(arr, 16));
+
+        System.out.println(binarySearchRecursion(arr,61,arr.length-1,0));
+        System.out.println(binarySearchRecursion(arr,38,arr.length-1,0));
     }
 
-    private static int binarySearch(int[] arr,int target){
-        int max = arr.length-1;
+    private static int binarySearch(int[] arr, int target) {
+        int max = arr.length - 1;
         int min = 0;
         int index = -1;
         int mid;
 
-        if (min>max||target<arr[min]||target>arr[max]){
+        if (min > max || target < arr[min] || target > arr[max]) {
             return index;
         }
 
-        while(min<=max){
-            mid = (min+max)/2;
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-            }
-
-            if (arr[mid]>target){
-                max = mid-1;
-            }else if (arr[mid]<target){
-                min = mid+1;
-            }else{
+        while (min <= max) {
+            mid = (min + max) / 2;
+            if (arr[mid] > target) {
+                max = mid - 1;
+            } else if (arr[mid] < target) {
+                min = mid + 1;
+            } else {
                 index = mid;
                 break;
             }
         }
         return index;
-
     }
+
+
+
+//    int max = arr.length-1;
+//    int min = 0;
+    /**
+     * 递归实现
+     */
+    private static int binarySearchRecursion(int[] arr, int target, int max, int min) {
+
+        int index = -1;
+        if (min > max || target < arr[min] || target > arr[max]) {
+            return index;
+        }
+        int mid = (min + max) / 2;
+        if (arr[mid] > target) {
+            index = binarySearchRecursion(arr, target, mid - 1, min);
+        } else if (arr[mid] < target) {
+            index = binarySearchRecursion(arr, target, max, mid + 1);
+        } else {
+            index = mid;
+        }
+        return index;
+    }
+
 }
